881ab448117516dcba87aa6dbcffbeb2cded8140
José Danubio de Araujo <danubio.bwm@gmail.com>
2025-09-22 13:58:49 -0300

    ajuste de testes subject service

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-preset: 'ts-jest',
-testEnvironment: 'node',
-moduleFileExtensions: ['js', 'json', 'ts'],
-rootDir: '.',
-testRegex: '.*\.spec\.ts$',
-transform: {
-'^.+\.ts$': 'ts-jest'
-}
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: '.',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.ts$': 'ts-jest'
+  }
 };
